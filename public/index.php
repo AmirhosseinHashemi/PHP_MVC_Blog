0629f9118aa78197646852d0a4be85cebed86ecb
Amirhossein <4mirh03einhashemi@gmail.com>
2024-12-05 22:18:39 +0330

    add other routes to the index

--- public/index.php
+++ public/index.php
@@ -11,6 +11,14 @@ $router = new Router($db);
 
 
 $router->get("/", [BlogsController::class, "index"]);
+$router->get("/blogs", [BlogsController::class, "index"]);
 
+$router->get("/blogs/create", [BlogsController::class, "create"]);
+$router->post("/blogs/create", [BlogsController::class, "create"]);
+
+$router->get("/blogs/update", [BlogsController::class, "update"]);
+$router->post("/blogs/update", [BlogsController::class, "update"]);
+
+$router->post("/blogs/delete", [BlogsController::class, "delete"]);
 
 $router->resolve();
