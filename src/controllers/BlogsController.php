5ab6671c995b1e2d49cbc50ed28eec3a6a47fee1
Amirhossein <4mirh03einhashemi@gmail.com>
2024-12-06 19:05:54 +0330

    implement create action for BlogController

--- src/controllers/BlogsController.php
+++ src/controllers/BlogsController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\Router;
+use app\models\Blog;
 
 class BlogsController
 {
@@ -11,7 +12,30 @@ class BlogsController
         $blogs = $router->db->get_blogs();
         $router->render_views('blogs/blogs', [
             'blogs' => $blogs,
-            "title" => "بلاگ ها"
+            "route" => "/"
+        ]);
+    }
+
+    public static function create(Router $router)
+    {
+        $blog_data = [];
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $blog_data["title"] = $_POST["title"];
+            $blog_data["text"] = $_POST["text"];
+            $blog_data["category"] = $_POST["category"];
+            $blog_data["image_file"] = $_FILES["image"];
+
+            $blog = new Blog();
+            $blog->load($blog_data);
+            $blog->save();
+
+            header("location: /blogs");
+            exit;
+        }
+
+        $router->render_views('blogs/create', [
+            "route" => "/blogs/create"
         ]);
     }
 }
