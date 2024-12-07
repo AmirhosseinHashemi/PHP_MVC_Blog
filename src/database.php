5ad75d638a0f746cde7b0885d8f53d94e368b77c
Amirhossein <4mirh03einhashemi@gmail.com>
2024-12-07 21:23:44 +0330

    add more operation for article table and one for categories table

--- src/database.php
+++ src/database.php
@@ -2,6 +2,7 @@
 
 namespace app;
 
+use app\models\Blog;
 use PDO;
 
 class Database
@@ -16,9 +17,56 @@ class Database
         self::$db = $this;
     }
 
+    // blogs table
     public function get_blogs()
     {
-        $query = $this->pdo->prepare("SELECT * FROM article");
+        $query = $this->pdo->prepare("SELECT article.*, categories.category_name
+                                         FROM article
+                                         JOIN categories ON article.category_id = categories.id");
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function create_blog(Blog $blog)
+    {
+        $query = $this->pdo->prepare("INSERT INTO article
+                                    (title, user_id, text, category_id, image_path)
+                                    VALUES 
+                                    (:title, :user_id, :text, :category_id, :image_path)");
+        $query->bindValue(":title", $blog->title);
+        $query->bindValue(":user_id", 1);
+        $query->bindValue(":text", $blog->text);
+        $query->bindValue(":category_id", $blog->category_id);
+        $query->bindValue(":image_path", $blog->image_path);
+
+        $query->execute();
+    }
+
+    public function update_blog(Blog $blog)
+    {
+        $query = $this->pdo->prepare("UPDATE article SET
+                                         title=:title, text=:text, category_id=:category_id,
+                                         image_path=:image_path WHERE id=:id");
+        $query->bindValue(":title", $blog->title);
+        $query->bindValue(":text", $blog->text);
+        $query->bindValue(":category_id", $blog->category_id);
+        $query->bindValue(":image_path", $blog->image_path);
+        $query->bindValue(":id", $blog->id);
+
+        $query->execute();
+    }
+
+    public function delete_blog(Blog $blog)
+    {
+        $query = $this->pdo->prepare("DELETE FROM article WHERE id=:id");
+        $query->bindValue(":id", $blog->id);
+        $query->execute();
+    }
+
+    // categories table
+    public function get_categories()
+    {
+        $query = $this->pdo->prepare("SELECT * FROM categories");
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
