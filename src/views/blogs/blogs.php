a0304208ef0ea39b17c2bf8485527bf3d278af1c
Amirhossein <4mirh03einhashemi@gmail.com>
2024-12-08 21:10:47 +0330

    change card style

--- src/views/blogs/blogs.php
+++ src/views/blogs/blogs.php
@@ -11,21 +11,20 @@
     </symbol>
 </svg>
 
-<div class="row justify-content-between">
+<div class="row mb-2">
     <?php foreach ($blogs as $blog) : ?>
-        <div class="col-5 g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-primary-emphasis"><?= $blog["category_name"] ?></strong>
-                <h3 class="mb-0"><?= $blog["title"] ?></h3>
-                <div class="mb-1 text-body-secondary">آذر</div>
-                <p class="card-text mb-auto"><?= substr($blog["text"], 0, 150) . '...' ?></p>
-                <a href="blog?id=<?= $blog["id"] ?>" class="btn btn-primary mt-3 align-self-end">
-                    ادامه
-                    <!-- <svg class="bi">
-                        <use xlink:href="#chevron-right"></use>
-                    </svg> -->
-                </a>
+        <div class="col-md-6">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-primary-emphasis"><?= $blog["category_name"] ?></strong>
+                    <h3 class="mb-0"><?= $blog["title"] ?></h3>
+                    <div class="mb-1 text-body-secondary">اذر 15</div>
+                    <p class="card-text mb-auto"><?= substr($blog["text"], 0, 150) . '...' ?></p>
+                    <a href="blog?id=<?= $blog["id"] ?>" class="btn btn-outline-primary mt-3 align-self-end">
+                        ادامه
+                    </a>
+                </div>
+
             </div>
-        </div>
-    <?php endforeach; ?>
+        </div> <?php endforeach; ?>
 </div>
